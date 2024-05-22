4a48bf94d28dd20e660e06f10caaaa230fed6fe5
Yoann Tourtellier <tourtellier.yoann@gmail.com>
2024-05-22 15:02:39 +0200

    ok

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,7 @@
+# commentaire
 class Review < ApplicationRecord
   belongs_to :restaurant
-  validates :content, :rating, presence: true
+  validates :content, presence: true
+  validates :rating, presence: true
   validates :rating, numericality: { only_integer: true }, inclusion: { in: (0..5) }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Restaurant.destroy_all
+
+Restaurant.create!(name: "restau1", address: "address1", phone_number: "01", category: "chinese")
+Restaurant.create!(name: "restau2", address: "address2", phone_number: "02", category: "italian")
+Restaurant.create!(name: "restau3", address: "address3", phone_number: "03", category: "japanese")
+Restaurant.create!(name: "restau4", address: "address4", phone_number: "04", category: "french")
+Restaurant.create!(name: "restau5", address: "address5", phone_number: "05", category: "belgian")
